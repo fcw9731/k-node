49974353667b7180a62c8cc92d59d35e277c2910
anasta-w <anasta@hotmail.co.nz>
2017-05-19 09:33:37 +0700

    Fix bugs

--- lib/alert_value.rb
+++ lib/alert_value.rb
@@ -15,42 +15,42 @@ module AlertValue
       item: item_params,
     }
 
-    $dynamodb.put_item(params)
+    # $dynamodb.put_item(params)
 
   end
 
   def attach_lambda_to_thing_rule(thing_name, type )
     thing_rule = thing_name + "_rule"
 
-    former_rule = $iot.get_topic_rule({
-      rule_name: thing_rule
-      })
+    # former_rule = $iot.get_topic_rule({
+    #   rule_name: thing_rule
+    #   })
 
-      dynamodb_rule = former_rule.rule.actions[0].dynamo_db.to_h
+    #   dynamodb_rule = former_rule.rule.actions[0].dynamo_db.to_h
 
-      lambda_arn = get_alert_type_lambda_arn(type).configuration.function_arn
+    #   lambda_arn = get_alert_type_lambda_arn(type).configuration.function_arn
 
-      lambda_params = {
-        function_arn: lambda_arn
-      }
+    #   lambda_params = {
+    #     function_arn: lambda_arn
+    #   }
 
-      params = {
-        rule_name: thing_rule,
-        topic_rule_payload: {
-          sql: "SELECT state.reported.data as data, state.reported.devEUI as devEUI, state.reported.ts as ts FROM '$aws/things/test12345/shadow/update'",
-          actions: [
-            {
-              dynamo_db: dynamodb_rule
-            },
-            lambda: {
-              function_arn: lambda_arn
-            }
-          ]
-        }
-      }
+    #   params = {
+    #     rule_name: thing_rule,
+    #     topic_rule_payload: {
+    #       sql: "SELECT state.reported.data as data, state.reported.devEUI as devEUI, state.reported.ts as ts FROM '$aws/things/test12345/shadow/update'",
+    #       actions: [
+    #         {
+    #           dynamo_db: dynamodb_rule
+    #         },
+    #         lambda: {
+    #           function_arn: lambda_arn
+    #         }
+    #       ]
+    #     }
+    #   }
 
       begin
-        $iot.replace_topic_rule(params)
+        # $iot.replace_topic_rule(params)
       end
   end
 
@@ -58,37 +58,37 @@ module AlertValue
 
     thing_rule = thing_name + "_rule"
 
-    former_rule = $iot.get_topic_rule({
-      rule_name: thing_rule
-      })
+    # former_rule = $iot.get_topic_rule({
+    #   rule_name: thing_rule
+    #   })
 
-      dynamodb_rule = former_rule.rule.actions[0].dynamo_db.to_h
+    #   dynamodb_rule = former_rule.rule.actions[0].dynamo_db.to_h
 
-      params = {
-        rule_name: thing_rule,
-        topic_rule_payload: {
-          sql: "SELECT state.reported.data as data, state.reported.devEUI as devEUI, state.reported.ts as ts FROM '$aws/things/test12345/shadow/update'",
-          actions: [
-            {
-              dynamo_db: dynamodb_rule
-            }
-          ]
-        }
-      }
+    #   params = {
+    #     rule_name: thing_rule,
+    #     topic_rule_payload: {
+    #       sql: "SELECT state.reported.data as data, state.reported.devEUI as devEUI, state.reported.ts as ts FROM '$aws/things/test12345/shadow/update'",
+    #       actions: [
+    #         {
+    #           dynamo_db: dynamodb_rule
+    #         }
+    #       ]
+    #     }
+    #   }
 
       begin
-        $iot.replace_topic_rule(params)
+        # $iot.replace_topic_rule(params)
       end
 
   end
 
   def get_alert_type_lambda_arn(type)
 
-    function_name = "alert_" + type
+    # function_name = "alert_" + type
 
-    return $lambda.get_function({
-      function_name: function_name
-      })
+    # return $lambda.get_function({
+    #   function_name: function_name
+    #   })
 
   end
 
@@ -107,7 +107,7 @@ module AlertValue
       }
     }
 
-    $dynamodb.update_item(params)
+    # $dynamodb.update_item(params)
 
   end
 
