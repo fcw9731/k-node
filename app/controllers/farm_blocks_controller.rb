0138d98a773e2af57c6d702c68a4fd6aa04c23a4
anasta <anasta@hotmail.co.nz>
2017-05-21 12:49:12 +0700

    Round number of Flow meter

--- app/controllers/farm_blocks_controller.rb
+++ app/controllers/farm_blocks_controller.rb
@@ -227,7 +227,7 @@ class FarmBlocksController < ApplicationController
 
       device_object[:latest_reading] = { timestamp: SensorData.convert_timestamp_to_datetime(losant_device_info['lastUpdated']) }
       # device_object[:data] = losant_device_state[0]['data']['totalflow']
-      device_object[:data] = losant_device_state[0]['data']['avgflowrate']
+      device_object[:data] = losant_device_state[0]['data']['avgflowrate'].round(2)
     end  
 
     # fb_devices[:inflow_meters].each do |device_object|
